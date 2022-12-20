861af90918c21e3a1b9afef093f209d3029e86fb
kikeai <lg5967133@gmail.com>
2022-12-20 10:45:34 -0400

    db models completed

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -5,25 +5,28 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Dog', {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
-      allowNull: false,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     height: {
-      type: DataTypes.STRING,
+      type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
     },
     weight: {
-      type: DataTypes.STRING,
+      type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
     },
     life_span: {
       type: DataTypes.STRING,
       defaultValue: "unknown"
+    },
+    image: {
+      type: DataTypes.STRING(500),
     }
   }, { timestamps: false });
 };
--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -4,12 +4,9 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Temperament', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   }, { timestamps: false });
 };
