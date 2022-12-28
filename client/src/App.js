2264a79dbfcbbf8e46d087626ceaa4ec193fa596
kikeai <lg5967133@gmail.com>
2022-12-28 18:45:17 -0400

    add navbar route

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import Home from './components/Home/Home';
 import { Route } from 'react-router-dom';
-
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
   return (
     <div>
       <Route exact path="/" render={() => <LandingPage />} />
+      <Route path="/home">
+        <NavBar />
+      </Route>
       <Route exact path="/home" render={() => <Home />} />
     </div>
   );
