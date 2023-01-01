220aef33bf024b772f9629791dcfbe9e5b1e5018
kikeai <lg5967133@gmail.com>
2023-01-01 15:24:55 -0400

    card component created

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,56 @@
+import styles from './card.module.css'
+import weightIcon from '../../media/peso.png'
+
+export default function Card(props){
+    const {name, weight, temperaments, image} = props
+    const weightToString = () => {
+        let weight1 = weight[0]
+        let weight2 = weight[1]
+        let weightStr;
+        if(weight1 === "NaN" && weight2 !== "NaN" && !!weight2){
+            weightStr = `( ${weight2} )`
+            return weightStr;
+        }
+        if(!weight2){
+            if(weight1 === "NaN") weight1 = "Unknown";
+            weightStr = `( ${weight1} )`;
+            return weightStr;
+        }else {
+            if(weight1 === "NaN") weight1 = "Unknown";
+            if(weight2 === "NaN") weight2 = "Unknown";
+            weightStr = `( ${weight1} - ${weight2} )`;
+            return weightStr;
+        }
+    }
+    const temperamentsToString = () => {
+        let temp1 = temperaments[0];
+        let temp2 = temperaments[1];
+        let tempStr;
+        if(!temp1){
+            tempStr = "Unknown"
+            return tempStr
+        }
+        if(!temp2){
+            tempStr = `(${temp1})`
+            return tempStr;
+        }else {
+            tempStr = `(${temp1}, ${temp2})`
+            return tempStr;
+        }
+    }
+    return (
+        <div className={styles.cardContainer}>
+            <img className={styles.dog} src={image} alt="perro" />
+            <div className={styles.nameContainer}>
+                <h4 className={styles.name}>{name}</h4>
+            </div>
+            <div className={styles.weightContainer}>
+                <img className={styles.weight} src={weightIcon} alt="icono" />
+                <p className={styles.weightText}>{weightToString()}</p>
+            </div>
+            <div className={styles.temperamentsContainer}>
+                <p className={styles.temperaments}>{temperamentsToString()}</p>
+            </div>
+        </div>
+    )
+}
