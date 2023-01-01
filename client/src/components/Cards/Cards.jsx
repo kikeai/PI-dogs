c0e3a5c80afd57552065627ae8d7cf2f716aaf65
kikeai <lg5967133@gmail.com>
2023-01-01 15:25:54 -0400

    Render cards component created

---
+++ client/src/components/Cards/Cards.jsx
@@ -0,0 +1,18 @@
+import styles from "./cards.module.css"
+import Card from "../Card/Card";
+
+export default function Cards(props){
+    const {dogs} = props
+    return(
+            <div className={styles.cards}>
+                {
+                    dogs.map((b) => <Card
+                        key={b.id}
+                        name={b.name}
+                        weight={b.weight}
+                        temperaments={b.temperaments}
+                        image= {b.image}/>)
+                }
+            </div>
+    )
+}
