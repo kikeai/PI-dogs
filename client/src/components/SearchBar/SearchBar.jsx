3203b221e9b3ab8787447e2c8afd769a6e53dffd
kikeai <lg5967133@gmail.com>
2023-01-01 15:30:57 -0400

    add filter breed

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,20 @@
 import styles from './searchbar.module.css'
 import lupa from '../../media/lupa.png'
+import { useDispatch } from 'react-redux';
+import * as actions from '../../redux/actions'
 
 
 export default function SearchBar(){
+    const dispatch = useDispatch();
+    function handleChange(e){
+        const {value} = e.target;
+        dispatch(actions.filterBreeds(value))
+    }
+    
 
     return(
         <div className={styles.container}>
-        <select className={styles.select}>
+        <select className={styles.select} name="filterbreed" onChange={handleChange}>
             <option value="All">All</option>
             <option value="Existent">Existent</option>
             <option value="Created">Created</option>
