9b2be5f62dc5e6af94287e1995ed03f66ef1bcad
kikeai <lg5967133@gmail.com>
2023-01-07 11:02:23 -0400

    add default value selects

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -26,7 +26,7 @@ export default function Filters(){
                 <h3 className={styles.filterTitles}>Filters</h3>
                 <p className={styles.nameSelects}>Temperament</p>
                 <select className={styles.filterSelects} name="temperament" onChange={handleChange}>
-                    <option disabled selected defaultValue>Select Temperament</option>
+                    <option value="" hidden>Select Temperament</option>
                     <option value="All">All</option>
                     {
                         temps.map(t => <option value={t.name} key={`${t.id}${t.name}`}>{t.name}</option>)
@@ -38,14 +38,14 @@ export default function Filters(){
                 <h3 className={styles.filterTitles}>Orders</h3>
                 <p className={styles.nameSelects}>Name</p>
                 <select className={styles.filterSelects} name="orderbreed" onChange={handleChange}>
-                    <option disabled selected defaultValue>Select Order</option>
+                    <option value="" hidden>Select Order</option>
                     <option value="A-Z">A-Z</option>
                     <option value="Z-A">Z-A</option>
                 </select>
 
                 <p className={styles.nameSelects}>Weigth</p>
                 <select className={styles.filterSelects} name="orderweigth" onChange={handleChange}>
-                    <option disabled selected defaultValue>Select Order</option>
+                    <option value="" hidden>Select Order</option>
                     <option value="Ascendent">Ascendent</option>
                     <option value="Descendent">Descendent</option>
                 </select>
