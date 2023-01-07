4f89f8cce0d8e09e1d145111d4d078dc2c17ca7d
kikeai <lg5967133@gmail.com>
2023-01-07 11:00:15 -0400

    eliminate disabled button

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -4,10 +4,7 @@ import lupa from '../../media/lupa.png'
 import { useDispatch } from 'react-redux';
 import * as actions from '../../redux/actions';
 
-const searchRegex = /[a-zA-Z]+$/
-
 export default function SearchBar(){
-    const [error, setError] = useState("")
     const [breed, setBreed] = useState("");
 
     const dispatch = useDispatch();
@@ -16,14 +13,9 @@ export default function SearchBar(){
         dispatch(actions.filterBreeds(value))
     }
 
-    function validate(e){
-        if(!searchRegex.test(e.target.value)){
-            setBreed(e.target.value)
-            setError("Not numbers, not special characters")
-        } else{
-            setError("")
-            setBreed(e.target.value)   
-        }
+    function handleWrite(e){
+        const {value} = e.target;
+        setBreed(value)
     }
     
     function handleSubmit(e){
@@ -40,8 +32,8 @@ export default function SearchBar(){
             <option value="Created">Created</option>
         </select>
         <form className={styles.search} onSubmit={handleSubmit}>
-            <input className={styles.input} type="search" value={breed} onChange={validate} placeholder="Search breed" />
-            <button disabled={!!error} className={styles.buttonSearch} type="submit">
+            <input className={styles.input} type="search" onChange={handleWrite} value={breed} placeholder="Search breed" />
+            <button className={styles.buttonSearch} type="submit">
                 <img className={styles.lens} src={lupa} alt="lens" />
             </button>
         </form>
