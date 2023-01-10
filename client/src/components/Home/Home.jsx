7398af1e2c50ba08e5443a60754ebae1dfc43a6b
kikeai <lg5967133@gmail.com>
2023-01-10 10:07:23 -0400

    add condition in useEffect and dispatch requestYoN

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -11,16 +11,20 @@ export default function Home(){
     const [pagina, setPagina] = useState (1);
     // eslint-disable-next-line no-unused-vars
     const [porPagina, setPorPagina] = useState (8);
-  
-    useEffect(() => {
-      dispatch(actions.getBreeds());
-      dispatch(actions.getTemperaments());
-    }, [dispatch]);
     
-    const breeds = useSelector((state) => state.myBreeds)
+    const req = useSelector((state) => state.request);
+    const breeds = useSelector((state) => state.myBreeds);
     const maximo = Math.ceil(breeds.length / porPagina);
     let dogs = breeds.slice((pagina - 1) * porPagina, (pagina - 1) * porPagina + porPagina);
 
+    useEffect(() => {
+        if(req){
+            dispatch(actions.getBreeds());
+            dispatch(actions.getTemperaments());
+            dispatch(actions.requestYoN(false));
+        }
+    }, [dispatch, req]);
+    
     return (
         <div className="home">
             <Filters />
