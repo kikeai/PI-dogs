139ea8f58b7f649ad048f7dc5351d9e7d36ec26b
kikeai <lg5967133@gmail.com>
2023-01-13 15:22:48 -0400

    delete devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import breeds from './reducer';
 
 const store = createStore(
    breeds,
-   composeWithDevTools(applyMiddleware(thunk))
+   applyMiddleware(thunk)
 );
 
 export default store;
